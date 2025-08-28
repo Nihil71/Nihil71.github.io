76f1d00804cee38a955f6e33d4142db2a6b58367
Nihil71 <144783541+Nihil71@users.noreply.github.com>
2025-08-28 20:00:05 +0800

    Add files via upload

---
+++ js/notes.js
@@ -0,0 +1,17 @@
+// js/notes.js
+// 笔记数据
+const notes = [
+    {
+        id: 1,
+        title: "Mathematical Physics",
+        subtitle: "参考书目：《Mathematical Physics》Sadri Hassani",
+        content: "",
+        category: "数学",
+        date: "2025-08-28",
+        site: "",
+        outline: [
+            { title: "1 Mathematical Preliminaries", comment: "" }
+
+        ]
+    }
+];
